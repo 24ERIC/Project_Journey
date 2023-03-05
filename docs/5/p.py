ba6469b2451db23ca1b951f00f51e2eca091bf5a
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-03-05 08:37:14 -0500

    update

--- docs/5/p.py
+++ docs/5/p.py
@@ -1,18 +1,22 @@
 f = open("./in.txt", "r")
 for line in f:
-    if line.replace("\n", "") != "":
-        line = line.replace("\n", "").replace("* ", "- ").replace("TryHackMe | ", "")
-        if line[0:3] == "---":
-            continue
-        elif line[0] == "-":
-            print(line.replace("\n", "").replace("- ", "            - "))
-        elif line[0] == "#":
-            print(line.replace("\n", "").replace("## ", "        - "))
-        
-        line = line.replace("            - [", "").replace(")", "")
-        if line.startswith("        - "):
-            continue
-        if "]" in line:
-            i = line.index("]")
-            line = line[i+2:]
+    if line != "" and line != "\n":
         print(line)
+# f = open("./in.txt", "r")
+# for line in f:
+#     if line.replace("\n", "") != "":
+#         line = line.replace("\n", "").replace("* ", "- ").replace("TryHackMe | ", "")
+#         if line[0:3] == "---":
+#             continue
+#         elif line[0] == "-":
+#             print(line.replace("\n", "").replace("- ", "            - "))
+#         elif line[0] == "#":
+#             print(line.replace("\n", "").replace("## ", "        - "))
+        
+#         line = line.replace("            - [", "").replace(")", "")
+#         if line.startswith("        - "):
+#             continue
+#         if "]" in line:
+#             i = line.index("]")
+#             line = line[i+2:]
+#         print(line)
